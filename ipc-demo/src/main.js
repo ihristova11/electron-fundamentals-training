df521ac0deb9e740a5a58b6b44373586109e6fb4
Irina Hristova <hristova119@gmail.com>
2018-06-05 20:14:46 +0300

    Updated main.js to display only one window

--- ipc-demo/src/main.js
+++ ipc-demo/src/main.js
@@ -6,31 +6,25 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain
 
-const windows = []
-
-let mainWindow
+let win
 
 app.on('ready', _ => {
-  [1, 2, 3].forEach(i => {
-    let win = new BrowserWindow({
+    win = new BrowserWindow({
       height: 400,
       width: 400
     })
+
     win.loadURL(`file://${__dirname}/countdown.html`)
     win.on('closed', _ => {
       win = null
     })
-    windows.push(win)
   })
-})
 
 ipc.on('countdown-start', _ => {
   console.log('starting!')
 
   countdown(count => {
     console.log("count", count)
-    windows.forEach(win => {
       win.webContents.send('countdown', count)
     })
   })
-})
