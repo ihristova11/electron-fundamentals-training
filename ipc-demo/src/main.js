1c5ccc15975a2ee66613ddc34526376d5937b3c7
Irina Hristova <hristova119@gmail.com>
2018-06-05 18:48:35 +0300

    Updated main.js and added countdown.html

--- ipc-demo/src/main.js
+++ ipc-demo/src/main.js
@@ -1,7 +1,18 @@
 const electron = require('electron')
 
 const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
 
 app.on('ready', _ => {
-    console.log('ready document!')
+    mainWindow = new BrowserWindow({
+        heigh: 300, 
+        width: 300
+    })
+
+    mainWindow.on('ready', _ => {
+        console.log('çlosed');
+        mainWindow = null
+    })
 })
