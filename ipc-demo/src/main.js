ccdd9327fe3b5897f4cb632ff2d66ddc0896a7db
Irina Hristova <hristova119@gmail.com>
2018-06-05 19:19:05 +0300

    Updated main.js and added countdown.html

--- ipc-demo/src/main.js
+++ ipc-demo/src/main.js
@@ -1,18 +1,36 @@
 const electron = require('electron')
 
+const countdown = require('./countdown')
+
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain
+
+const windows = []
 
 let mainWindow
 
 app.on('ready', _ => {
-    mainWindow = new BrowserWindow({
-        heigh: 300, 
-        width: 300
+  [1, 2, 3].forEach(i => {
+    let win = new BrowserWindow({
+      height: 400,
+      width: 400
     })
+    win.loadURL(`file://${__dirname}/countdown.html`)
+    win.on('closed', _ => {
+      win = null
+    })
+    windows.push(win)
+  })
+})
 
-    mainWindow.on('ready', _ => {
-        console.log('çlosed');
-        mainWindow = null
+ipc.on('countdown-start', _ => {
+  console.log('starting!')
+
+  countdown(count => {
+    console.log("count", count)
+    windows.forEach(win => {
+      win.webContents.send('countdown', count)
     })
+  })
 })
